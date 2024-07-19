0f4e3d703aa561f4bf1a7be10b7ad6aa8b01ff8c
Mohamed Bahaa <MohamedElzohary2001@outlook.com>
2024-07-19 04:21:45 +0300

    fixed transcription job

--- app/jobs/process_transcription_job.rb
+++ app/jobs/process_transcription_job.rb
@@ -9,15 +9,16 @@ class ProcessTranscriptionJob < ApplicationJob
     project.processing!
     File.binwrite(Rails.root.join("tmp", project_id.to_s), project.file.download)
 
-    transcription = TRANSCRIBER.transcribe_audio(Rails.root.join("tmp", project_id.to_s)
+    transcription = TRANSCRIBER.transcribe_audio(Rails.root.join("tmp", project_id.to_s))
     if transcription && project.update(transcription: transcription)
       project.completed!
     else
       project.failed!
       ProcessTranscriptionJob.set(wait: 10.seconds).perform_later(project_id)
     end
-
-    # TODO: rescue Transcriber
+  rescue Transcriber::NotAvailable
+    project.failed!
+    ProcessTranscriptionJob.set(wait: 10.seconds).perform_later(project_id)
   ensure
     FileUtils.rm_rf(Rails.root.join("tmp", project_id.to_s))
   end
